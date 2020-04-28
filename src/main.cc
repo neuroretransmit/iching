5ce4637d8be1af064db8d6f2dbf087e07bce2789
John Holly <j@optimistic.ninja>
2020-04-28 16:02:44 -0700

    Print key to stderr so file redirection works, allow file input for encode/decode via -i flag.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sys/stat.h>
 
 #include <boost/program_options.hpp>
 
@@ -17,24 +19,58 @@ enum {
   ERROR_UNHANDLED_EXCEPTION
 };
 
-static void help(const po::options_description& desc) {
+static inline bool file_exists(const string& fname)
+{
+  struct stat buffer;
+  return (stat (fname.c_str(), &buffer) == 0); 
+}
+
+static void help(const po::options_description& desc)
+{
     cout << "I Ching Hexagram Encoder/Decoder" << std::endl << desc << std::endl;
 }
 
-static void encode_msg(po::variables_map vm) {
+static void encode_msg(po::variables_map vm)
+{
     Translator translator = Translator();
     bool with_substition_cipher = vm["substitution-cipher"].as<bool>();
-    cout << translator.encode(vm["encode"].as<string>(), with_substition_cipher) << endl;
+    bool with_input_file = vm["input"].as<bool>();
+    string plaintext = vm["encode"].as<string>();
+    
+    if (with_input_file) {
+        string fname = vm["encode"].as<string>();
+        std::ifstream ifs(fname);
+        
+        if (file_exists(fname)) {
+            string file_contents((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+            plaintext = file_contents;
+        } else {
+            cerr << "ERROR: File '" << fname << "' does not exist.\n";
+            exit(ERROR_IN_COMMAND_LINE);
+        }
+    }
+    
+    cout << translator.encode(plaintext, with_substition_cipher) << endl;
 }
 
-static void decode_msg(po::variables_map vm) {
+static void decode_msg(po::variables_map vm)
+{
     Translator translator = Translator();
     bool keyed = vm.count("key") || vm.count("k");
     
+    // Set encoded to fi
+    string encoded = vm["decode"].as<string>();
+    
+    if (vm.count("input") || vm.count("i")) {
+        std::ifstream ifs(vm["decode"].as<string>());
+        string file_contents((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+        encoded = file_contents;
+    }
+    
     if (keyed) {
-        cout << translator.decode(vm["decode"].as<string>(), vm["key"].as<string>()) << endl;
+        cout << translator.decode(encoded, vm["key"].as<string>()) << endl;
     } else {
-        cout << translator.decode(vm["decode"].as<string>()) << endl;
+        cout << translator.decode(encoded) << endl;
     }
 }
 
@@ -44,6 +80,7 @@ int main(int argc, char** argv)
     string decode = "";
     string key = "";
     bool substitution_cipher = false;
+    bool file = false;
     
     try { 
         po::options_description desc("Options"); 
@@ -52,12 +89,13 @@ int main(int argc, char** argv)
             ("encode,e", po::value(&encode), "Encode message") 
             ("decode,d", po::value(&decode), "Decode message")
             ("substitution-cipher,s", po::bool_switch(&substitution_cipher), "Use substitution cipher")
+            ("input,i", po::bool_switch(&file), "With input file")
             ("key,k", po::value(&key), "Key for decoding substitution cipher"); 
         po::variables_map vm; 
         
         try { 
             po::store(po::parse_command_line(argc, argv, desc), vm);
-    
+            
             if (vm.count("help") || vm.count("h")) { 
                 help(desc);
             } else if (vm.count("encode") || vm.count("e")) {
